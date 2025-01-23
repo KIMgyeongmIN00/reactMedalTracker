ab5a25204a48e8ef0eb616b4347f48244c8e0a23
KGyeong <rlarudals61@naver.com>
2025-01-23 19:12:31 +0900

    edit.App.jsx_로직종속

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,16 @@
 import React, { useState } from "react";
-import Title from "./components/Title";
 import Content from "./components/Content";
 import ListCountry from "./components/ListCountry";
 
 const App = () => {
   const [countries, setCountries] = useState([]);
-
-  // 국가 삭제 핸들러
-  const handleDelete = (id) => {
-    // 국가 삭제
-    const deletedCountries = countries.filter((country) => country.id !== id);
-    setCountries(deletedCountries);
-
-    // 삭제할 국가 찾고
-    const countryToDelete = countries.find((country) => country.id === id);
-
-    // 삭제된 국가 이름을 알림창에 표시
-    alert(`${countryToDelete.countryName} 국가를 삭제했습니다.`);
-  };
+  const LOCAL_STORAGE = "olympicEntryCountries";
 
   return (
     <>
-      <Title />
+      <h1>2024 파리 올림픽 메달 기록</h1>
       <Content countries={countries} setCountries={setCountries} />
-      <ListCountry countries={countries} handleDelete={handleDelete} />
+      <ListCountry countries={countries} setCountries={setCountries} />
     </>
   );
 };
