6d7a17039bf322117ab980f6a0e0557c0a007c97
KGyeong <rlarudals61@naver.com>
2025-01-23 19:08:15 +0900

    edit.Content.jsx_인풋컴포넌트변경

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,15 +1,11 @@
-import CountryInput from "./CountryInput";
-import MedalInput from "./MedalInput";
+import Inputs from "./Inputs";
 import Buttons from "./Buttons";
-import ListCountry from "./ListCountry";
 import { useState } from "react";
 
 const Content = ({ countries, setCountries }) => {
-  const [countryName, setCountryName] = useState("");
-  const [medals, setMedals] = useState({
-    gold: 0,
-    silver: 0,
-    bronze: 0,
+  const [medalsInfo, setMedalsInfo] = useState({
+    countryName: "",
+    medals: { gold: 0, silver: 0, bronze: 0 },
   });
 
   // 국가 찾는 함수
@@ -22,10 +18,10 @@ const Content = ({ countries, setCountries }) => {
     e.preventDefault();
 
     //중복 검사
-    const isCountryFound = findCountryByName(countryName);
+    const isCountryFound = findCountryByName(medalsInfo.countryName);
 
     // 유효성 검사
-    if (!countryName.trim()) {
+    if (!medalsInfo.countryName.trim()) {
       alert("국가 이름을 입력해주세요.");
       return;
     }
@@ -37,17 +33,23 @@ const Content = ({ countries, setCountries }) => {
 
     const newCountry = {
       id: new Date().getTime(),
-      countryName: countryName,
-      goldMedal: medals.gold,
-      silverMedal: medals.silver,
-      bronzeMedal: medals.bronze,
+      countryName: medalsInfo.countryName,
+      goldMedal: Number(medalsInfo.medals.gold),
+      silverMedal: Number(medalsInfo.medals.silver),
+      bronzeMedal: Number(medalsInfo.medals.bronze),
     };
 
     setCountries([...countries, newCountry]);
 
     // 인풋 내부 초기화
-    setCountryName("");
-    setMedals({ gold: 0, silver: 0, bronze: 0 });
+    setMedalsInfo({
+      countryName: "",
+      medals: {
+        gold: 0,
+        silver: 0,
+        bronze: 0,
+      },
+    });
   };
 
   // 국가 업데이트 핸들러
@@ -55,23 +57,23 @@ const Content = ({ countries, setCountries }) => {
     e.preventDefault();
 
     // 유효성 검사
-    if (!countryName.trim()) {
+    if (!medalsInfo.countryName.trim()) {
       alert("국가 이름을 입력해주세요.");
       return;
     }
 
     //일치자 찾기
-    const isCountryFound = findCountryByName(countryName);
+    const isCountryFound = findCountryByName(medalsInfo.countryName);
 
     if (isCountryFound) {
       // 국가가 존재하면 업데이트
       const updatedCountries = countries.map((country) => {
-        if (country.countryName === countryName) {
+        if (country.countryName === medalsInfo.countryName) {
           return {
             ...country,
-            goldMedal: medals.gold,
-            silverMedal: medals.silver,
-            bronzeMedal: medals.bronze,
+            goldMedal: Number(medalsInfo.medals.gold),
+            silverMedal: Number(medalsInfo.medals.silver),
+            bronzeMedal: Number(medalsInfo.medals.bronze),
           };
         }
         return country;
@@ -80,45 +82,67 @@ const Content = ({ countries, setCountries }) => {
       // 업데이트된 배열로 상태 업데이트
       setCountries(updatedCountries);
 
-      // 입력 필드 초기화
-      setCountryName("");
-      setMedals({ gold: 0, silver: 0, bronze: 0 });
+      // 인풋 내부 초기화
+      setMedalsInfo({
+        countryName: "",
+        medals: {
+          gold: 0,
+          silver: 0,
+          bronze: 0,
+        },
+      });
 
-      alert(`${countryName} 메달 데이터를 업데이트했습니다.`);
+      alert(`${medalsInfo.countryName} 메달 데이터를 업데이트했습니다.`);
     } else {
       // 국가가 존재하지 않으면 경고 메시지
-      alert(`${countryName}에 해당하는 국가가 없습니다!`);
+      alert(`${medalsInfo.countryName}에 해당하는 국가가 없습니다!`);
     }
   };
 
-  // 메달 state 객체 핸들러
+  // 국가 이름 변경 핸들러
+  const handleCountryNameChange = (value) => {
+    setMedalsInfo((beforeCountryName) => ({
+      ...beforeCountryName,
+      countryName: value,
+    }));
+  };
+
+  // 메달 개수 변경 핸들러
   const handleMedalChange = (type, value) => {
-    setMedals((beforeMedals) => ({
+    setMedalsInfo((beforeMedals) => ({
       ...beforeMedals,
-      [type]: value,
+      medals: {
+        ...beforeMedals.medals,
+        [type]: value,
+      },
     }));
   };
 
   return (
     <>
       <form onSubmit={handleFormSubmit}>
-        <CountryInput
-          countryName={countryName}
-          setCountryName={setCountryName}
+        <Inputs
+          label="국가이름"
+          type="text"
+          value={medalsInfo.countryName}
+          onChange={handleCountryNameChange}
         />
-        <MedalInput
-          medalType="금메달"
-          value={medals.gold}
+        <Inputs
+          label="금메달"
+          type="number"
+          value={medalsInfo.medals.gold}
           onChange={(value) => handleMedalChange("gold", value)}
         />
-        <MedalInput
-          medalType="은메달"
-          value={medals.silver}
+        <Inputs
+          label="은메달"
+          type="number"
+          value={medalsInfo.medals.silver}
           onChange={(value) => handleMedalChange("silver", value)}
         />
-        <MedalInput
-          medalType="동메달"
-          value={medals.bronze}
+        <Inputs
+          label="동메달"
+          type="number"
+          value={medalsInfo.medals.bronze}
           onChange={(value) => handleMedalChange("bronze", value)}
         />
         <Buttons
