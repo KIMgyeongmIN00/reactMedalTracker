45cb68fd159b33f6fde439bd0596afcd1584c8bf
KGyeong <rlarudals61@naver.com>
2025-01-23 16:37:56 +0900

    Update.App.jsx_컴포넌트재분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import React, { useState } from "react";
 import Title from "./components/Title";
 import Content from "./components/Content";
+import ListCountry from "./components/ListCountry";
 
 const App = () => {
+  const [countries, setCountries] = useState([]);
+
+  // 국가 삭제 핸들러
+  const handleDelete = (id) => {
+    // 국가 삭제
+    const deletedCountries = countries.filter((country) => country.id !== id);
+    setCountries(deletedCountries);
+
+    // 삭제할 국가 찾고
+    const countryToDelete = countries.find((country) => country.id === id);
+
+    // 삭제된 국가 이름을 알림창에 표시
+    alert(`${countryToDelete.countryName} 국가를 삭제했습니다.`);
+  };
+
   return (
     <>
       <Title />
-      <Content />
+      <Content countries={countries} setCountries={setCountries} />
+      <ListCountry countries={countries} handleDelete={handleDelete} />
     </>
   );
 };
